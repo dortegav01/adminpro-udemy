b44d39beae6e2d869786167646024ded038e1004
dortegav <dortegav@minsait.com>
2020-01-02 13:30:16 +0100

    include ngModel for manual value in input of progress component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import {Graficas1Component} from './graficas1/graficas1.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {PagesComponent} from './pages.component';
 import {SharedModule} from '../shared/shared.module';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import {SharedModule} from '../shared/shared.module';
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule
   ],
   exports: [
     DashboardComponent,
